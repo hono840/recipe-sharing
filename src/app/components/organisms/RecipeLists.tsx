9f0fb846f9cfbd2c277b9b5e9a35373008b85f4a
Hiroki Ono <h.ono@xtone.co.jp>
2025-02-24 15:39:04 +0900

    レシピ取得

--- src/app/components/organisms/RecipeLists.tsx
+++ src/app/components/organisms/RecipeLists.tsx
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RecipeTitle from "../atoms/RecipeTitle";
 import SmallText from "../atoms/SmallText";
 import LikesCount from "../atoms/LikesCount";
 import RecipeListCard from "./RecipeListCard";
+import { supabase } from "@/app/utils/supabaseClient";
+
+interface Recipe {
+  id: string;
+  created_at: string;
+  name: string;
+  description: string;
+  user_id: string;
+  image_url: string;
+}
 
 const RecipeLists = () => {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const fetchRecipes = async () => {
+    try {
+      const { data, error } = await supabase.from("recipes").select("*");
+      if (error) {
+        console.error(error);
+      }
+      setRecipes(data || []);
+      console.log(data);
+    } catch {}
+  };
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
   return (
     <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {/* サンプルデータ（動的に変更予定） */}
-      {[...Array(6)].map((_, i) => (
-        <RecipeListCard key={i}>
+      {recipes.map((reicpe) => (
+        <RecipeListCard key={reicpe.id}>
           <img
             src="/images/placeholder.png"
             alt="レシピ画像"
             className="w-full h-48 object-cover rounded-md"
           />
-          <RecipeTitle>レシピタイトル{i + 1}</RecipeTitle>
-          <SmallText>投稿者: ユーザー名</SmallText>
+          <RecipeTitle>{reicpe.name}</RecipeTitle>
+          <SmallText>投稿者: 投稿者</SmallText>
           <div className="flex justify-between items-center mt-2">
             <LikesCount likesCount={10} />
             <a
